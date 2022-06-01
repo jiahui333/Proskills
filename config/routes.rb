8b4449bcd8d51fbdaaccfff080bfe2528d8125ff
jiahui <forestlin333@gmail.com>
2022-06-01 17:53:12 +0200

    landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get "components", to: "pages#components"
   get "ui-kit", to: 'pages#ui'
-  get "/skills", to: "pages#index"
+  get "skills", to: "pages#index"
+  get "jobs", to: "jobs#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
