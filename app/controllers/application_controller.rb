7e5a76b3c925c5ffd095f610b092cfe72361f52c
Carolina Stahl <stahl@langeit.com>
2022-06-02 17:14:50 +0200

    Forms finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :full_name, :student])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :full_name, :student])
   end
 end
