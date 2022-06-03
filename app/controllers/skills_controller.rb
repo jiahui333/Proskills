d52457c9791d259d9af8477b4eb3129a027835b2
jiahui <forestlin333@gmail.com>
2022-06-03 16:46:28 +0200

    implement seeds on the skill show page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,6 +6,14 @@ class SkillsController < ApplicationController
 
   def show
     @skill = Skill.find(params[:id])
+    @resources = []
+    @skill.resources.each do |r|
+        @resources << eval(r)
+    end
+
+    @beginner_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'beginner' }
+    @intermediate_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'intermediate' }
+    @advanced_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'advanced' }
   end
 
 end
