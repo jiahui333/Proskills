da72f53085b7419b028c1ef763b36e4c8f79a791
Eric Stahl <ericstahl8@gmail.com>
2022-06-06 17:37:56 +0200

    search almost finished

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,34 +1,20 @@
 class SkillsController < ApplicationController
 
+  def show
+    @skill = Skill.find(params[:id])
+  end
+
   def index
     @skills = Skill.all
     if params[:query].present?
       if params[:query] == "frontend"
-        @skills = Skills.where("title ILIKE ?", "%#{params[:query]}%")
+        @skills = Skill.where(frontend: true)
       end
+
+      if params[:query] == "backend"
+        @skills = Skill.where(backend: true)
+      end
+
     end
   end
-
-  def show
-    @skill = Skill.find(params[:id])
-    @resources = []
-    @skill.resources.each do |r|
-        @resources << eval(r)
-    end
-
-    @beginner_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'beginner' }
-    @intermediate_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'intermediate' }
-    @advanced_resources = @resources.select { |res_hash| res_hash[:difficulties] == 'advanced' }
-  end
-
 end
-
-
-#   <%= form_tag skills_path, method: :get do %>
-#   <%= text_field_tag :query,
-#     params[:query],
-#     class: "form-control",
-#     placeholder: "Choose your skills"
-#   %>
-#   <%= submit_tag "Search", class: "btn btn-primary" %>
-# <% end %>
