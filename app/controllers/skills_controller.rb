64739824856bebf57321a03ba4f2b7908e2c720b
Eric Stahl <ericstahl8@gmail.com>
2022-06-06 12:34:45 +0200

    test

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -6,5 +6,19 @@ class SkillsController < ApplicationController
 
   def index
     @skills = Skill.all
-  end
+    if params[:query].present?
+      if params[:query] == "frontend"
+        @skills = Skills.where("title ILIKE ?", "%#{params[:query]}%")
+      end
+    end
 end
+
+
+#   <%= form_tag skills_path, method: :get do %>
+#   <%= text_field_tag :query,
+#     params[:query],
+#     class: "form-control",
+#     placeholder: "Choose your skills"
+#   %>
+#   <%= submit_tag "Search", class: "btn btn-primary" %>
+# <% end %>
