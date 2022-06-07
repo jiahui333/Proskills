7fe10e8523100cbfc87d04378450bdc0c3b03b03
Carolina Stahl <stahl@langeit.com>
2022-06-07 17:17:11 +0200

    Sign up flown done

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   get "components", to: "pages#components"
   get "ui-kit", to: 'pages#ui'
   get "/skills", to: "skills#index"
-  get "/stepone", to: "pages#stepone"
   get "/steptwo", to: "pages#steptwo", as: :steptwo
   get "dashboard", to: "pages#dashboard"
-  # get "skills/:id", to: "skills#show"
+  get "dashboard/skills", to: "pages#dashboardmyskills"
+  get "dashboard/jobs", to: "pages#dashboardmyjobs"
   resources :skills, only: [:index, :show]
   resources :jobs, only: [:index, :show]
+  get "/user_edit", to: "pages#user_update"
 
-  patch "/user_edit", to: "pages#user_update"
+  # get "skills/:id", to: "skills#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
