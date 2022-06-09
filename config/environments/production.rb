9b3ad355614b483d7fa535100cfb27a50610fcf4
jiahui <forestlin333@gmail.com>
2022-06-09 14:15:27 +0200

    mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "proskills_production"
 
   config.action_mailer.perform_caching = false
+  ActionMailer::Base.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'gmail.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
+}
+
+  Rails.application.configure do
+
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.default_url_options = { host: "proskillsss.herokuapp.com" }
+    # or your custom domain name eg. "www.yourdomain.com"
+  end
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
